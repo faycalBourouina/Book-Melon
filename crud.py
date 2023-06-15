110cc88c3b093b96875a0b25f46265368ebb4ef4
Faycal <faycalbourouina@gmail.com>
2023-06-15 13:07:33 -0500

    format_datetime: splitted in two seprate functions for date and time

--- crud.py
+++ crud.py
@@ -1,5 +1,5 @@
 from mode import User, Reservation
-from utils import format_datetime
+from utils import format_date, format_time
 
 def create_user(username):
     """ Create and return a new user """
@@ -38,22 +38,19 @@ def add_reservation(username, reservation_date, start_time, end_time):
     """Add a reservation to the user's reservations."""
     
     # Format the date and time strings into datetime objects
-    reservation_date = format_datetime(reservation_date, start_time)
-    start_time = format_datetime(reservation_date, start_time)
-    end_time = format_datetime(reservation_date, end_time)
+    reservation_date = format_date(reservation_date)
+    start_time = format_time(reservation_date)
+    end_time = format_time(reservation_date)
 
  # Check if any reservation exists on the specified date and time range
     existing_reservation = Reservation.query.filter(
         Reservation.date == reservation_date,
-        Reservation.start_date <= start_time,
-        Reservation.end_date >= end_time
+        Reservation.start_date == start_time,
+        Reservation.end_date == end_time,
     ).first()
 
     if existing_reservation:
-        return None
-
-    # Find the user
-    user = User.query.filter_by(username=username).first()
+        return "reservation_exists"
 
 
     # Create a new reservation
--- utils.py
+++ utils.py
@@ -1,7 +1,11 @@
 from datetime import datetime
 
-def format_datetime(date_str, time_str):
-    """Formats the date and time strings into a datetime object."""
+def format_date(date_str):
+    """Formats the date string into a datetime object."""
     date = datetime.strptime(date_str, "%Y-%m-%d").date()
+    return date
+
+def format_time(time_str):
+    """Formats the time string into a datetime object."""
     time = datetime.strptime(time_str, "%H:%M").time()
-    return datetime.combine(date, time)
+    return time
